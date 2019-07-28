cf31c01f8033aa4b307b4943c9b55d145aa47e4f
Andre Costa <andrecostaa@gmail.com>
2019-07-27 22:09:12 -0300

    Funcionando a alteracao de cargos na lista cargos.

--- src/main/java/com/estudo/curso/boot/web/controller/CargoController.java
+++ src/main/java/com/estudo/curso/boot/web/controller/CargoController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -51,6 +52,19 @@ public class CargoController {
 	public String salvar(Cargo cargo, RedirectAttributes attr) {
 		cargoService.salvar(cargo);
 		attr.addFlashAttribute(SUCCESS, "Cargo inserido com sucesso.");
+		return "redirect:/cargos/cadastrar";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("cargo", cargoService.buscarPorId(id));
+		return "cargo/cadastro";
+	}
+	
+	@PostMapping("/editar")
+	public String editar(Cargo cargo, RedirectAttributes attr) {
+		cargoService.editar(cargo);
+		attr.addFlashAttribute(SUCCESS, "Registro atualizado com sucesso.");
 		return "redirect:/cargos/cadastrar";
 	}
 	
