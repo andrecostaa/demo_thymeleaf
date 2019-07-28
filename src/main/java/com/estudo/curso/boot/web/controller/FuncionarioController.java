53c3f932e4056e4905dd5a7e60554ddf6c0016a6
Andre Costa <andrecostaa@gmail.com>
2019-07-27 23:00:10 -0300

    Funcionalidade de cadastro de funcionarios.

--- src/main/java/com/estudo/curso/boot/web/controller/FuncionarioController.java
+++ src/main/java/com/estudo/curso/boot/web/controller/FuncionarioController.java
@@ -3,9 +3,21 @@
  */
 package com.estudo.curso.boot.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.estudo.curso.boot.domain.Cargo;
+import com.estudo.curso.boot.domain.Funcionario;
+import com.estudo.curso.boot.domain.UF;
+import com.estudo.curso.boot.service.CargoService;
+import com.estudo.curso.boot.service.FuncionarioService;
 
 /**
  * @author andrecosta
@@ -16,14 +28,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/funcionarios")
 public class FuncionarioController {
 
+	private static final String SUCCESS = "success";
+	private static final String FAIL = "fail";
+	
+	@Autowired
+	private FuncionarioService funcionarioService;
+	@Autowired
+	private CargoService cargoService;
+	
+	
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Funcionario funcionario) {
 		return "/funcionario/cadastro";
 	}
 	
 	@GetMapping("/listar")
 	public String listar() {
 		return "/funcionario/lista";
+	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Funcionario funcionario, RedirectAttributes attr) {
+		funcionarioService.salvar(funcionario);
+		attr.addAttribute(SUCCESS, "Funcionário inserido com sucesso.");
+		return "redirect:/funcionarios/cadastrar";
+	}
+	
+	@ModelAttribute("cargos")
+	public List<Cargo> getCargos() {
+		return cargoService.buscarTodos();
+	}
+	
+	@ModelAttribute("ufs")
+	public UF[] getUFs() {
+		return UF.values();
 	}
 	
 }
